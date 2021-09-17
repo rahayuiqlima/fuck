defc335d57ee26117be834f2eddfeeadfe059acf
rezakojexs <80284781+rezakojexs@users.noreply.github.com>
2021-09-17 21:42:12 +0700

    Update update-1.2.sh

--- update-1.2.sh
+++ update-1.2.sh
@@ -62,6 +62,7 @@ echo "0 0 * * * root xp" > /etc/crontab
 cd
 echo "1.3" > /home/ver
 clear
+sudo hostnamectl set-hostname SETUP-BY-THIRASTORE
 echo " Fix minor Bugs"
 echo " Now You Can Change Port Of Some Services"
 echo " Reboot 3 Sec"
