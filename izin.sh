4e39d75f00aee98a87cf1878245cad9eef908f98
rezakojexs <80284781+rezakojexs@users.noreply.github.com>
2021-09-17 17:22:28 +0700

    Update izin.sh

--- izin.sh
+++ izin.sh
@@ -2,7 +2,7 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-IZIN=$(curl https://raw.githubusercontent.com/Dork96/rentScript/main/ipvps | grep $MYIP)
+IZIN=$(curl https://raw.githubusercontent.com/rezakojexs/fuck/main/ipvps | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
 clear
 echo -e ""
