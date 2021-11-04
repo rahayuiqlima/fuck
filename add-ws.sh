b7add2df51e9453d69d112a9da1446a7037c5a54
rezakojexs <80284781+rezakojexs@users.noreply.github.com>
2021-11-04 20:08:01 +0700

    Update add-ws.sh

--- add-ws.sh
+++ add-ws.sh
@@ -50,7 +50,7 @@ cat>/etc/v2ray/$user-none.json<<EOF
       {
       "v": "2",
       "ps": "${user}",
-      "add": "your_bug.com",
+      "add": "104.17.2.81",
       "port": "${none}",
       "id": "${uuid}",
       "aid": "2",
