59a626be7ac7bfe0518fb3c6044c1c64c2a9f480
rezakojexs <80284781+rezakojexs@users.noreply.github.com>
2021-11-08 15:01:44 +0700

    Update websock.sh

--- websock.sh
+++ websock.sh
@@ -4,10 +4,10 @@ cd
 
 figlet -f slant Install WS | lolcat
 # Install Template
-wget -q -O /usr/local/bin/ws-drop "https://raw.githubusercontent.com/Dork96/Final/main/ws-dropbear.py"
-wget -q -O /usr/local/bin/ws-openssh "https://raw.githubusercontent.com/Dork96/Final/main/ws-openssh.py"
-wget -q -O /usr/local/bin/ws-ovpn "https://raw.githubusercontent.com/Dork96/Final/main/ws-ovpn.py"
-wget -q -O /usr/local/bin/ws-tls "https://raw.githubusercontent.com/Dork96/Final/main/ws-tls.py"
+wget -q -O /usr/local/bin/ws-drop "https://raw.githubusercontent.com/rezakojexs/fuck/main/ws-dropbear.py"
+wget -q -O /usr/local/bin/ws-openssh "https://raw.githubusercontent.com/rezakojexs/fuck/main/ws-openssh.py"
+wget -q -O /usr/local/bin/ws-ovpn "https://raw.githubusercontent.com/rezakojexs/fuck/main/ws-ovpn.py"
+wget -q -O /usr/local/bin/ws-tls "https://raw.githubusercontent.com/rezakojexs/fuck/main/ws-tls.py"
 
 chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-openssh
